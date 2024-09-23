2906f4bc94d6e36b5089e100778f7b9f223751f6
Annamalai Prabu <apfossdev@gmail.com>
2024-09-23 13:05:43 +0530

    Finish Pokemon Card Display Web App

--- week-3/easy/The-Pokémon/script.js
+++ week-3/easy/The-Pokémon/script.js
@@ -1,18 +1,6 @@
 const typeDropdown = document.getElementById("type");
 const inputBox = document.getElementById("input-box-id");
 
-function submitFunctionality() {
-  // send fetch request to poke api with above parameters (type and number), go to type, setup counter, count that many pokemons and their links store in an array
-    const typeValue = typeDropdown.value;
-    const inputBoxValue = parseInt(inputBox.value);
-    const pokemonList = getPokemonList(typeValue, inputBoxValue);
-    console.log(pokemonList);
-    
-  // clear screen and load these pokemon data from these array links
-
-  
-}
-
 async function getPokemonList(typeValue, inputBoxValue) {
     const pokemonList = [];
     try{
@@ -26,5 +14,58 @@ async function getPokemonList(typeValue, inputBoxValue) {
     }
     catch(error){
         console.error("Error fetching data:",error);
-        }
+    }
 }
+
+async function getPokemonData(pokemonList, incrementCounter){
+  try {
+    const apiURL = pokemonList[incrementCounter];
+    const response = await axios.get(apiURL);
+    const pokemonData = response.data;
+    return pokemonData;
+    // console.log(response.data);
+  }
+  catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
+
+async function submitFunctionality() {
+  // send fetch request to poke api with above parameters (type and number), go to type, setup counter, count that many pokemons and their links store in an array
+  const typeValue = typeDropdown.value;
+  const inputBoxValue = parseInt(inputBox.value);
+  const pokemonList = await getPokemonList(typeValue, inputBoxValue); //use await always to return required stuff from async functions
+  console.log(pokemonList);
+
+  // clear screen and load these pokemon data from these array links
+  const parentNode = document.getElementById("main");
+  parentNode.innerHTML = "";
+
+  for (let i = 0; i < inputBoxValue; i++) {
+    //fetch each card data from each url and update dom with each iteration
+    const pokemonData = await getPokemonData(pokemonList, i);
+    // console.log(pokemonData);
+    //add cards to the dom here
+    const cardDiv = document.createElement('div');
+    const pokemonNameDisplay = document.createElement('h3');
+    const pokemonName = pokemonData.name.charAt(0).toUpperCase() + pokemonData.name.slice(1);
+    pokemonNameDisplay.innerHTML = `Name: ${pokemonName}`;
+    const experienceDisplay = document.createElement('h4');
+    experienceDisplay.innerHTML = `Base Experience: ${pokemonData.base_experience}`;
+    const heightDisplay = document.createElement('h4');
+    heightDisplay.innerHTML = `Height: ${pokemonData.height}`;
+    const weightDisplay = document.createElement('h4');
+    weightDisplay.innerHTML = `Weight: ${pokemonData.weight}`;
+
+    //append these to cardDiv, cardDiv to parentNode
+
+    cardDiv.appendChild(pokemonNameDisplay);
+    cardDiv.appendChild(experienceDisplay);
+    cardDiv.appendChild(heightDisplay);
+    cardDiv.appendChild(weightDisplay);
+    parentNode.appendChild(cardDiv);
+  }
+
+}
+
+
